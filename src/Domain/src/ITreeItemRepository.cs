d2a0f209fd74535bbb0fabce9e5f7d8b56f9d845
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-11-12 18:34:51 +0300

    Add tree item model and repository

---
+++ src/Domain/src/ITreeItemRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Redplcs.Wizardsoft.Domain;
+
+public interface ITreeItemRepository
+{
+	void Create(TreeItem item);
+	TreeItem GetAll();
+	TreeItem GetById(Guid id);
+	void Update(TreeItem item);
+	void Delete(TreeItem item);
+}
---
+++ src/Domain/src/TreeItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Redplcs.Wizardsoft.Domain;
+
+public class TreeItem
+{
+	public Guid Id { get; set; }
+	public TreeItem? Parent { get; set; }
+	public List<TreeItem> Children { get; set; } = [];
+}
