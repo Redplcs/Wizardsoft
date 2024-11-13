018c18945d6988e8b7d6b0fda0edae8fb949b549
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-11-13 22:38:38 +0300

    Register entity framework in-memory provider and repository

--- src/Application/src/Program.cs
+++ src/Application/src/Program.cs
@@ -1,9 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Redplcs.Wizardsoft.Database;
+using Redplcs.Wizardsoft.Domain;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationContext>(options =>
+{
+	options.UseInMemoryDatabase(databaseName: "Database");
+});
+builder.Services.AddScoped<ITreeItemRepository, TreeItemRepository>();
 
 var app = builder.Build();
 
--- src/Database/src/ApplicationContext.cs
+++ src/Database/src/ApplicationContext.cs
@@ -3,7 +3,7 @@ using Redplcs.Wizardsoft.Domain;
 
 namespace Redplcs.Wizardsoft.Database;
 
-public class ApplicationContext : DbContext
+public class ApplicationContext(DbContextOptions options) : DbContext(options)
 {
 	public DbSet<TreeItem> Items => Set<TreeItem>();
 }
