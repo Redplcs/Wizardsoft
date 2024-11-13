dbf0abe8f16ac8d4ef2078332a8ac8debab46bc1
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-11-13 22:46:40 +0300

    Hide constructor behind static method

--- src/Domain/src/TreeItem.cs
+++ src/Domain/src/TreeItem.cs
@@ -2,7 +2,21 @@
 
 public class TreeItem
 {
+	private TreeItem()
+	{
+	}
+
 	public Guid Id { get; set; }
 	public TreeItem? Parent { get; set; }
 	public List<TreeItem> Children { get; set; } = [];
+
+	public static TreeItem Create(Guid? id = null, TreeItem? parent = null, IEnumerable<TreeItem>? children = null)
+	{
+		return new TreeItem
+		{
+			Id = id ?? Guid.NewGuid(),
+			Parent = parent,
+			Children = new List<TreeItem>(children ?? [])
+		};
+	}
 }
