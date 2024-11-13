fa504d49f7780a30e038be9125eee785ac69d140
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-11-13 22:48:34 +0300

    Clean up hosting configuration a little bit

---
+++ src/Application/src/WebApplicationExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Redplcs.Wizardsoft.Application;
+
+public static class WebApplicationExtensions
+{
+	public static WebApplication MapCrudOperations(this WebApplication app)
+	{
+		return app;
+	}
+}
