fcb4db6a68864e8d07c3885f66cafa6585dd132e
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-11-13 23:09:21 +0300

    Hide service registration behind extensions class

--- src/Application/src/Program.cs
+++ src/Application/src/Program.cs
@@ -1,18 +1,10 @@
-using Microsoft.EntityFrameworkCore;
 using Redplcs.Wizardsoft.Application;
-using Redplcs.Wizardsoft.Database;
-using Redplcs.Wizardsoft.Domain;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<ApplicationContext>(options =>
-{
-	options.UseInMemoryDatabase(databaseName: "Database");
-});
-builder.Services.AddScoped<ITreeItemRepository, TreeItemRepository>();
+builder.Services.AddInMemoryDatabase(databaseName: "TreeItem's database");
 
 var app = builder.Build();
 
