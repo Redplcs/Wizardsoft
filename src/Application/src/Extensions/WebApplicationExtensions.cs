282829188ec6857770e9924fa008cb44f54ac910
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-11-14 13:40:37 +0300

    Add additional parameters to Create operation

--- src/Application/src/Extensions/WebApplicationExtensions.cs
+++ src/Application/src/Extensions/WebApplicationExtensions.cs
@@ -16,13 +16,44 @@ public static class WebApplicationExtensions
 		return app;
 	}
 
-	private static TreeItem CreateHandler(
+	private static IResult CreateHandler(
+		[FromQuery] Guid? id,
+		[FromQuery(Name = "parent")] Guid? parentId,
+		[FromQuery(Name = "children")] Guid[]? childrenIds,
 		[FromServices] ITreeItemRepository repository)
 	{
-		var item = TreeItem.Create();
+		var item = TreeItem.Create(id);
+
+		if (parentId.HasValue)
+		{
+			var parent = repository.GetById(parentId.Value);
+
+			if (parent is null)
+			{
+				return Results.NotFound();
+			}
+
+			item.Parent = parent;
+		}
+
+		if (childrenIds is not null)
+		{
+			foreach (var childGuid in childrenIds)
+			{
+				var child = repository.GetById(childGuid);
+
+				if (child is null)
+				{
+					return Results.NotFound();
+				}
+
+				item.Children.Add(child);
+			}
+		}
+
 		repository.Create(item);
 
-		return item;
+		return Results.Ok(item);
 	}
 
 	private static IEnumerable<TreeItem> GetAllHandler(
