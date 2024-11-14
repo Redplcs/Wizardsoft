6a45ca05e19df67f6de20920ff417dad6b563af6
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-11-14 12:02:02 +0300

    Implement read operation

--- src/Application/src/Extensions/WebApplicationExtensions.cs
+++ src/Application/src/Extensions/WebApplicationExtensions.cs
@@ -1,34 +1,45 @@
-﻿namespace Redplcs.Wizardsoft.Application.Extensions;
+﻿using Microsoft.AspNetCore.Mvc;
+using Redplcs.Wizardsoft.Domain;
+
+namespace Redplcs.Wizardsoft.Application.Extensions;
 
 public static class WebApplicationExtensions
 {
-    public static WebApplication MapGetCrudEndpoints(this WebApplication app)
-    {
-        app.MapGet("/create", CreateHandler);
-        app.MapGet("/read", ReadHandler);
-        app.MapGet("/update", UpdateHandler);
-        app.MapGet("/delete", DeleteHandler);
+	public static WebApplication MapGetCrudEndpoints(this WebApplication app)
+	{
+		app.MapGet("/create", CreateHandler);
+		app.MapGet("/get-all", GetAllHandler);
+		app.MapGet("/get-by-id", GetByIdHandler);
+		app.MapGet("/update", UpdateHandler);
+		app.MapGet("/delete", DeleteHandler);
 
-        return app;
-    }
+		return app;
+	}
 
-    private static void CreateHandler()
-    {
-        throw new NotImplementedException();
-    }
+	private static void CreateHandler()
+	{
+		throw new NotImplementedException();
+	}
 
-    private static void ReadHandler()
-    {
-        throw new NotImplementedException();
-    }
+	private static IEnumerable<TreeItem> GetAllHandler([FromServices] ITreeItemRepository repository)
+	{
+		return repository.GetAll();
+	}
 
-    private static void UpdateHandler()
-    {
-        throw new NotImplementedException();
-    }
+	private static TreeItem? GetByIdHandler(
+		[FromQuery] Guid id,
+		[FromServices] ITreeItemRepository repository)
+	{
+		return repository.GetById(id);
+	}
 
-    private static void DeleteHandler()
-    {
-        throw new NotImplementedException();
-    }
+	private static void UpdateHandler()
+	{
+		throw new NotImplementedException();
+	}
+
+	private static void DeleteHandler()
+	{
+		throw new NotImplementedException();
+	}
 }
